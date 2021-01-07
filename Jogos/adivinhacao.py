43547e7adcacadfe6d005294a237e55608f73d94
MateusAlberto <madof007@gmail.com>
2021-01-07 11:44:48 -0300

    Python 3 parte 1: Aula 7

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -5,7 +5,19 @@ print("Bem vindo(a) ao jogo de Adivinhação!")
 print("************************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -21,13 +33,15 @@ for rodada in range(1, total_de_tentativas + 1):
     menor   = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!", end="\n\n")
+        print(f'Você acertou e fez {pontos} pontos!', end="\n\n")
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.", end="\n\n")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.", end="\n\n")
+        pontos_perdidos = abs(numero_secreto - chute) 
+        pontos = pontos - pontos_perdidos
 
 print("************************************")
 print("Fim do jogo")
