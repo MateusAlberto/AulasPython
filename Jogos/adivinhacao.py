defe7edc18815c0a7effe030deb418b067104ff4
MateusAlberto <madof007@gmail.com>
2021-01-07 09:26:03 -0300

    Python 3 parte 1: Aula 3

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -8,10 +8,17 @@ chute = int(input("Digite o seu número: "))
 
 print("Você digitou", chute)
 
-if (numero_secreto == chute):
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!", end="\n\n")
 else:
-    print("Você errou!", end="\n\n")
+    if (maior):
+        print("Você errou! O seu chute foi maior que o número secreto.", end="\n\n")
+    elif (menor):
+        print("Você errou! O seu chute foi menor que o número secreto.", end="\n\n")
 
 print("************************************")
 print("Fim do jogo")
