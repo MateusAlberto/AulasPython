445bc81ec0393eb7cba88a76de4e0853cee42449
MateusAlberto <madof007@gmail.com>
2021-01-07 13:36:53 -0300

    Python 3 parte 2: Aula 1

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,6 +3,15 @@ def jogar():
     print("Bem vindo(a) ao jogo de Forca!")
     print("************************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou e não acertou
+    while(not enforcou and not acertou):
+        print("jogando...")
+
     print("************************************")
     print("Fim do jogo")
     print("************************************")
