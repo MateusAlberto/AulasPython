cb474ffead95035ad7ba04bba866faeeee7feb59
MateusAlberto <madof007@gmail.com>
2021-01-07 10:05:45 -0300

    Python 3 parte 1: Aula 5

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -4,12 +4,15 @@ print("************************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print("Você digitou", chute)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!", end="\n\n")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -17,13 +20,12 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!", end="\n\n")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.", end="\n\n")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.", end="\n\n")
-    
-    rodada += 1
 
 print("************************************")
 print("Fim do jogo")
