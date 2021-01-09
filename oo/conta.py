8c5d31c3e3d5738376569d958771cbb54227f868
MateusAlberto <madof007@gmail.com>
2021-01-09 14:14:24 -0300

    Python 3 intro OO: Aula 4

--- oo/conta.py
+++ oo/conta.py
@@ -1,23 +1,28 @@
 class Conta:
     # Construtor da classe
     def __init__(self, numero, titular, saldo = 0.0, limite = 1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     # Função que gera o extrato bancário de uma conta
     def extrato(self):
         print("****** EXTRATO BANCÁRIO ******")
-        print(f'Número da Conta: {self.numero}')
-        print(f'Titular da Conta: {self.titular}')
-        print(f'Saldo da Conta: {self.saldo}')
+        print(f'Número da Conta: {self.__numero}')
+        print(f'Titular da Conta: {self.__titular}')
+        print(f'Saldo da Conta: {self.__saldo}')
         print("******************************")
 
     # Função para depositar um valor em uma conta
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     # Função para sacar o valor de uma conta
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    # Função para realizar uma transferência
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
