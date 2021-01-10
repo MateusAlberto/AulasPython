f35ad8553400b5becae260769237d22192b2efcd
MateusAlberto <madof007@gmail.com>
2021-01-10 13:08:36 -0300

    Python 3 intro OO: Aula 6

--- oo/conta.py
+++ oo/conta.py
@@ -8,10 +8,6 @@ class Conta:
 
     # Gets e sets
     @property
-    def numero(self):
-        return self.__numero
-
-    @property
     def titular(self):
         return self.__titular
 
@@ -27,6 +23,14 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
+
     # Função que gera o extrato bancário de uma conta
     def extrato(self):
         print("****** EXTRATO BANCÁRIO ******")
@@ -39,9 +43,17 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    # Função que verifica se um determinado valor pode ser sacado
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     # Função para sacar o valor de uma conta
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f'O valor {valor} passou o limite')
 
     # Função para realizar uma transferência
     def transfere(self, valor, destino):
