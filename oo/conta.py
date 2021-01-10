49c106a1f51e7745e0ad7dd70662755efa460978
MateusAlberto <madof007@gmail.com>
2021-01-10 12:36:32 -0300

    Python 3 intro OO: Aula 5

--- oo/conta.py
+++ oo/conta.py
@@ -6,6 +6,27 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
+    # Gets e sets
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
     # Função que gera o extrato bancário de uma conta
     def extrato(self):
         print("****** EXTRATO BANCÁRIO ******")
